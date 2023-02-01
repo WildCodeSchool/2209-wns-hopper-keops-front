4d05236785c4e1026e07b610bbca3f7bd59fe86e
g-emeline <emelineg@polydea.com>
2023-02-01 16:42:49 +0100

    started add action button to challenge

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -1,15 +1,14 @@
-import { IAction } from '../interfaces/IAction';
+import { useState } from "react";
+import { IAction } from "../interfaces/IAction";
 
 const ActionCard = (props: IAction) => {
-	function addAction() {}
-
-	return (
-		<div>
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-			<button onClick={addAction}>Ajouter</button>
-		</div>
-	);
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      {/* <button onClick={addAction}>Ajouter</button> */}
+    </div>
+  );
 };
 
 export default ActionCard;
--- src/components/ActionsChallenge.tsx
+++ src/components/ActionsChallenge.tsx
@@ -1,29 +1,37 @@
-import { useQuery } from '@apollo/client';
-import React, { useContext } from 'react';
-import { ChallengeContext } from '../context/CreateChallengeProvider';
-import { readAllActions } from '../graphql/readAllActions';
-import { IChallengeNavigation } from '../interfaces/IChallengeNavigation';
-import ActionCard from './ActionCard';
-import { IAction } from '../interfaces/IAction';
+import { useQuery } from "@apollo/client";
+import React, { useContext, useState } from "react";
+import { ChallengeContext } from "../context/CreateChallengeProvider";
+import { readAllActions } from "../graphql/readAllActions";
+import { IChallengeNavigation } from "../interfaces/IChallengeNavigation";
+import ActionCard from "./ActionCard";
+import { IAction } from "../interfaces/IAction";
 
 const ActionsChallenge = ({ setChallengeNavigation }: IChallengeNavigation) => {
-	const challengeData = useContext(ChallengeContext);
-	console.log('this is challenge contexte :', challengeData);
+  const challengeData = useContext(ChallengeContext);
+  console.log("this is challenge contexte :", challengeData);
 
-	const { data } = useQuery<{ readAllActions: IAction[] }>(readAllActions);
+  const { data } = useQuery<{ readAllActions: IAction[] }>(readAllActions);
+  console.log(data);
 
-	console.log(data);
-	return (
-		<div>
-			<h2>ActionsChallenge</h2>
-			{data?.readAllActions.map(action => (
-				<ActionCard key={action.id} {...action} />
-			))}
-			<button onClick={() => setChallengeNavigation('invitation')}>
-				Actions
-			</button>
-		</div>
-	);
+  const [actionsList, setActionsList] = useState<IAction["id"][]>([]);
+  const [challengeActions, setChallengeActions] = useState<string[]>([]);
+  const addAction = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const id = event.currentTarget.id;
+    setActionsList([...actionsList]);
+    // challengeActions.push(event.currentTarget.id);
+    console.log(event.currentTarget.id);
+  };
+  return (
+    <div>
+      <h2>ActionsChallenge</h2>
+      {data?.readAllActions.map((action) => (
+        <ActionCard key={action.id} {...action} />
+      ))}
+      <button onClick={() => setChallengeNavigation("invitation")}>
+        Actions
+      </button>
+    </div>
+  );
 };
 
 export default ActionsChallenge;
