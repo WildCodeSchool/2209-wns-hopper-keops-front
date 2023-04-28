d722a878cabd4cf6cc994939d08e719ec1263598
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2023-04-28 12:19:28 +0200

    added useState to modify challenge information

--- src/components/ActionTile.tsx
+++ src/components/ActionTile.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { IAction } from "../interfaces/IAction";
+import React from 'react';
+import { IAction } from '../interfaces/IAction';
 
-const ActionTile = (props: { action: IAction }) => {
-  return (
-    <li key={props.action.id}>
-      <h6>{props.action.title}</h6>
-      <div>{/* truc à cocher */}</div>
-    </li>
-  );
+const ActionTile = (props: { action: IAction; length: number }) => {
+	const checkboxes = [];
+
+	for (let i = 0; i < props.length; i++) {
+		checkboxes.push(
+			<div key={i}>
+				Jour {i + 1} : <input type="checkbox" />
+			</div>,
+		);
+	}
+
+	return (
+		<li key={props.action.id}>
+			<h6>{props.action.title}</h6>
+			<div>{checkboxes}</div>
+		</li>
+	);
 };
 
 export default ActionTile;
--- src/pages/ChallengePage.tsx
+++ src/pages/ChallengePage.tsx
@@ -1,175 +1,213 @@
-import { useMutation, useQuery } from "@apollo/client";
-import React, { useContext, useEffect, useState } from "react";
-import { IParticipantChallenge } from "../interfaces/IChallenge";
-import { IUser } from "../interfaces/IUser";
-import { IAction } from "../interfaces/IAction";
-import { readOneChallenge } from "../graphql/readOneChallenge";
-import { Navigate, useParams } from "react-router-dom";
+import { useMutation, useQuery } from '@apollo/client';
+import React, { useContext, useEffect, useState } from 'react';
+import { IParticipantChallenge } from '../interfaces/IChallenge';
+import { IUser } from '../interfaces/IUser';
+import { IAction } from '../interfaces/IAction';
+import { readOneChallenge } from '../graphql/readOneChallenge';
+import { Navigate, useParams } from 'react-router-dom';
 
-import format from "date-fns/format";
-import { createUserToChallenge } from "../graphql/createUserToChallenge";
-import { UserContext } from "../context/AuthProvider";
-import { removeUserToChallenge } from "../graphql/removeUserToChallenge";
-import { deleteMyChallenge } from "../graphql/deleteMyChallenge";
-import { useNavigate } from "react-router-dom";
-import { readMyChallenges } from "../graphql/readMyChallenges";
-import ActionsList from "../components/ActionsList";
+import format from 'date-fns/format';
+import { createUserToChallenge } from '../graphql/createUserToChallenge';
+import { UserContext } from '../context/AuthProvider';
+import { removeUserToChallenge } from '../graphql/removeUserToChallenge';
+import { deleteMyChallenge } from '../graphql/deleteMyChallenge';
+import { useNavigate } from 'react-router-dom';
+import { readMyChallenges } from '../graphql/readMyChallenges';
+import ActionsList from '../components/ActionsList';
 
 const ChallengePage = () => {
-  const navigate = useNavigate();
+	const [challengeIsEditable, setChallengeIsEditable] =
+		useState<boolean>(false);
+	const navigate = useNavigate();
 
-  const user = useContext(UserContext);
+	const user = useContext(UserContext);
 
-  const { challengeId } = useParams();
+	const { challengeId } = useParams();
 
-  const [challenge, setChallenge] = useState<
-    null | IParticipantChallenge | undefined
-  >(undefined);
+	const [challenge, setChallenge] = useState<
+		null | IParticipantChallenge | undefined
+	>(undefined);
 
-  const [userStatus, setUserStatus] = useState<null | "participant" | "owner">(
-    null
-  );
+	const [userStatus, setUserStatus] = useState<null | 'participant' | 'owner'>(
+		null,
+	);
 
-  const [userToChallengeId, setUserToChallengeId] = useState<string>("");
+	const [userToChallengeId, setUserToChallengeId] = useState<string>('');
 
-  const [createUserToChallengeMutation, { error: createUserToChallengeError }] =
-    useMutation(createUserToChallenge);
+	const [createUserToChallengeMutation, { error: createUserToChallengeError }] =
+		useMutation(createUserToChallenge);
 
-  const [removeUserToChallengeMutation, { error: removeUserToChallengeError }] =
-    useMutation(removeUserToChallenge);
+	const [removeUserToChallengeMutation, { error: removeUserToChallengeError }] =
+		useMutation(removeUserToChallenge);
 
-  const [deleteMyChallengeMutation, { error: deleteMyChallengeError }] =
-    useMutation(deleteMyChallenge, { refetchQueries: [readMyChallenges] });
+	const [deleteMyChallengeMutation, { error: deleteMyChallengeError }] =
+		useMutation(deleteMyChallenge, { refetchQueries: [readMyChallenges] });
 
-  const { data } = useQuery<{ readOneChallenge: IParticipantChallenge }>(
-    readOneChallenge,
-    {
-      variables: {
-        challengeId,
-      },
-    }
-  );
+	const { data } = useQuery<{ readOneChallenge: IParticipantChallenge }>(
+		readOneChallenge,
+		{
+			variables: {
+				challengeId,
+			},
+		},
+	);
 
-  useEffect(() => {
-    if (data) {
-      setChallenge(data?.readOneChallenge);
-    }
-    console.log("DATA users:", data);
+	// Question à poser à Aurélien
 
-    data?.readOneChallenge.userToChallenges.map((userToChallenge) => {
-      const userId = userToChallenge.user.id;
+	function calculateDaysRemaining() {
+		const today = new Date();
 
-      if (userId === user?.id) {
-        setUserStatus("participant");
-        setUserToChallengeId(userToChallenge.id);
+		const challengeStartDate = challenge?.start_date
+			? new Date(challenge.start_date)
+			: undefined;
 
-        console.log("UserToChallengeId:", userToChallengeId);
+		if (challengeStartDate) {
+			// Calcul en millisecondes entre aujourd'hui et la date de début du challenge
+			const timeDiff = challengeStartDate.getTime() - today.getTime();
 
-        if (data.readOneChallenge.createdBy.id === user?.id) {
-          setUserStatus("owner");
-        }
-      }
-      console.log("User Status:", userStatus);
-    });
-  }, [data, user?.id, userStatus]);
+			// Calcul du nombre de jours restants (1000 = nombre de millisecondes dans une seconde, 3600 secondes dans une heure)
+			const milisecondInADay = 1000 * 3600 * 24;
+			// Math.ceil arrondi à l'entier supérieur
+			const daysRemaining = Math.ceil(timeDiff / milisecondInADay);
 
-  function shareChallenge() {
-    let url = document.location.href;
+			if (daysRemaining === 0) {
+				return 'Le challenge est en cours !';
+			}
 
-    navigator.clipboard.writeText(url).then(
-      function () {
-        console.log("Copied!");
-      },
-      function () {
-        console.log("Copy error");
-      }
-    );
-  }
+			return `Patience ! Le challenge commence dans ${daysRemaining} jours`;
+		} else {
+			console.log('La date de début du challenge est manquante.');
+		}
+	}
 
-  const participateToChallenge = async () => {
-    try {
-      await createUserToChallengeMutation({
-        variables: {
-          isAccepted: true,
-          challengeId,
-        },
-      });
+	useEffect(() => {
+		if (data) {
+			setChallenge(data?.readOneChallenge);
+		}
 
-      console.log("Participation OK");
-      setUserStatus("participant");
-    } catch (error) {
-      console.log("error :", error);
-    }
-  };
+		data?.readOneChallenge.userToChallenges.map(userToChallenge => {
+			const userId = userToChallenge.user.id;
 
-  const quitChallenge = async () => {
-    console.log("THIS IS userToChallenge :", userToChallengeId);
-    try {
-      await removeUserToChallengeMutation({
-        variables: {
-          data: {
-            userToChallengeId,
-          },
-        },
-      });
+			if (userId === user?.id) {
+				setUserStatus('participant');
+				setUserToChallengeId(userToChallenge.id);
 
-      console.log("Quit the challenge");
-    } catch (error) {
-      console.log("error from quit challenge:", error);
-    }
-  };
+				console.log('UserToChallengeId:', userToChallengeId);
 
-  const deleteChallenge = async () => {
-    try {
-      await deleteMyChallengeMutation({
-        variables: {
-          challengeId,
-        },
-      });
-      console.log("Challenge removed !");
-      navigate("/dashboard", { replace: true });
-    } catch (error) {
-      console.log("error :", error);
-    }
-  };
+				if (data.readOneChallenge.createdBy.id === user?.id) {
+					setUserStatus('owner');
+				}
+			}
+			console.log('User Status:', userStatus);
+		});
+	}, [data, user?.id, userStatus]);
 
-  if (challenge === null) {
-    return <Navigate to={"/dashboard"} replace={true} />;
-  } else if (challenge !== undefined && challenge !== null) {
-    return (
-      <div>
-        <h2>{challenge.name}</h2>
-        {
-          <p>
-            Date de début:{" "}
-            {format(new Date(challenge.start_date), "yyyy-MM-dd")}
-          </p>
-        }
-        <p>Durée : {challenge.length}</p>
-        <p>Créé par : {challenge.createdBy.id}</p>
+	function shareChallenge() {
+		let url = document.location.href;
 
-        <button onClick={shareChallenge}>Partager ce challenge</button>
+		navigator.clipboard.writeText(url).then(
+			function () {
+				console.log('Copied!');
+			},
+			function () {
+				console.log('Copy error');
+			},
+		);
+	}
 
-        {userStatus === null ? (
-          <button onClick={participateToChallenge}>Je participe !</button>
-        ) : userStatus === "owner" ? (
-          <button onClick={deleteChallenge}>Supprimer le challenge</button>
-        ) : (
-          <button onClick={quitChallenge}>Quitter le challenge</button>
-        )}
+	const participateToChallenge = async () => {
+		try {
+			await createUserToChallengeMutation({
+				variables: {
+					isAccepted: true,
+					challengeId,
+				},
+			});
 
-        <h4>Participants:</h4>
-        <li>
-          {challenge.userToChallenges.map((participant: { user: IUser }) => {
-            return <p key={participant.user.id}>{participant.user.name}</p>;
-          })}
-        </li>
-        <ActionsList challenge={challenge} />
-      </div>
-    );
-  } else {
-    return <div>Loading...</div>;
-  }
+			console.log('Participation OK');
+			setUserStatus('participant');
+		} catch (error) {
+			console.log('error :', error);
+		}
+	};
+
+	const quitChallenge = async () => {
+		console.log('THIS IS userToChallenge :', userToChallengeId);
+		try {
+			await removeUserToChallengeMutation({
+				variables: {
+					data: {
+						userToChallengeId,
+					},
+				},
+			});
+
+			console.log('Quit the challenge');
+		} catch (error) {
+			console.log('error from quit challenge:', error);
+		}
+	};
+
+	const updateChallenge = async () => {
+		console.log('modifier le challenge');
+	};
+
+	const deleteChallenge = async () => {
+		try {
+			await deleteMyChallengeMutation({
+				variables: {
+					challengeId,
+				},
+			});
+			console.log('Challenge removed !');
+			navigate('/dashboard', { replace: true });
+		} catch (error) {
+			console.log('error :', error);
+		}
+	};
+
+	if (challenge === null) {
+		return <Navigate to={'/dashboard'} replace={true} />;
+	} else if (challenge !== undefined && challenge !== null) {
+		return (
+			<div>
+				<h2>{challenge.name}</h2>
+				{
+					<p>
+						Date de début:{' '}
+						{format(new Date(challenge.start_date), 'yyyy-MM-dd')}
+					</p>
+				}
+				<p>Durée : {challenge.length}</p>
+				<p>Créé par : {challenge.createdBy.id}</p>
+
+				<button onClick={shareChallenge}>Partager ce challenge</button>
+
+				{userStatus === null ? (
+					<button onClick={participateToChallenge}>Je participe !</button>
+				) : userStatus === 'owner' ? (
+					<>
+						<button onClick={deleteChallenge}>Supprimer le challenge</button>
+						<button onClick={updateChallenge}>Modifier le challenge</button>
+					</>
+				) : (
+					<button onClick={quitChallenge}>Quitter le challenge</button>
+				)}
+
+				<h4>Participants:</h4>
+				<li>
+					{challenge.userToChallenges.map((participant: { user: IUser }) => {
+						return <p key={participant.user.id}>{participant.user.name}</p>;
+					})}
+				</li>
+
+				{calculateDaysRemaining()}
+				<ActionsList challenge={challenge} />
+			</div>
+		);
+	} else {
+		return <div>Loading...</div>;
+	}
 };
 
 export default ChallengePage;
