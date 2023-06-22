0113784a0ab511951c9115960953ba1917428002
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2023-06-22 10:01:21 +0200

    added new modifications

--- src/components/ActionsChallenge.tsx
+++ src/components/ActionsChallenge.tsx
@@ -1,51 +1,51 @@
-import { useQuery } from "@apollo/client";
-import { readAllActions } from "../graphql/readAllActions";
-import ActionCard from "./ActionCard";
-import { IAction } from "../interfaces/IAction";
+import { useQuery } from '@apollo/client';
+import { readAllActions } from '../graphql/readAllActions';
+import ActionCard from './ActionCard';
+import { IAction } from '../interfaces/IAction';
 
 const ActionsChallenge = ({
-  setChallengeNavigation,
-  setActionsList,
-  actionsList,
+	setChallengeNavigation,
+	setActionsList,
+	actionsList,
 }: {
-  setChallengeNavigation: (navigation: string) => void;
-  setActionsList: (actions: Array<{ id: string; title: string }>) => void;
-  actionsList: Array<{ id: string; title: string }>;
+	setChallengeNavigation: (navigation: string) => void;
+	setActionsList: (actions: Array<{ id: string; title: string }>) => void;
+	actionsList: Array<{ id: string; title: string }>;
 }) => {
-  const { data } = useQuery<{ readAllActions: IAction[] }>(readAllActions);
+	const { data } = useQuery<{ readAllActions: IAction[] }>(readAllActions);
 
-  const addAction = (action: IAction) => {
-    setActionsList([...actionsList, { id: action.id, title: action.title }]);
-  };
+	const addAction = (action: IAction) => {
+		setActionsList([...actionsList, { id: action.id, title: action.title }]);
+	};
 
-  console.log("This is actionList", actionsList);
+	console.log('This is actionList', actionsList);
 
-  return (
-    <div className="challengeContainer">
-      <h2>Choisis des action à réaliser</h2>
-      {data?.readAllActions.map((action) => (
-        <ActionCard
-          key={action.id}
-          {...action}
-          onClick={() => addAction(action)}
-        />
-      ))}
-      <div className="container-button-multiple">
-        <button
-          className="nextBtn button-inline"
-          onClick={() => setChallengeNavigation("initialize")}
-        >
-          Précédent
-        </button>
-        <button
-          className="nextBtn button-inline"
-          onClick={() => setChallengeNavigation("invitation")}
-        >
-          Suivant
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="challengeContainer">
+			<h2>Choisis des action à réaliser</h2>
+			{data?.readAllActions.map(action => (
+				<ActionCard
+					key={action.id}
+					{...action}
+					onClick={() => addAction(action)}
+				/>
+			))}
+			<div className="container-button-multiple">
+				<button
+					className="nextBtn button-inline"
+					onClick={() => setChallengeNavigation('initialize')}
+				>
+					Précédent
+				</button>
+				<button
+					className="nextBtn button-inline"
+					onClick={() => setChallengeNavigation('invitation')}
+				>
+					Suivant
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ActionsChallenge;
