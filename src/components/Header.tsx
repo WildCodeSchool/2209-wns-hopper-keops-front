1f69fad254a9620c8a93fe0b268af4cebe21f273
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2023-07-17 15:39:17 +0200

    hide navigation when 'editable' option is enalbled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,66 +1,64 @@
-import { Link } from "react-router-dom";
-import { UserContext, useToken } from "../context/AuthProvider";
-import { useContext } from "react";
-import logo from "../logoEpikEco.png";
-import "./Header.css";
+import { Link } from 'react-router-dom';
+import { UserContext, useToken } from '../context/AuthProvider';
+import { useContext } from 'react';
+import logo from '../logoEpikEco.png';
+import './Header.css';
 import { useState } from 'react';
 
 const Header = () => {
-  const user = useContext(UserContext);
-  const tokenContext = useToken();
-  const [showNotification, setShowNotification] = useState(false);
+	const user = useContext(UserContext);
+	const tokenContext = useToken();
+	const [showNotification, setShowNotification] = useState(false);
 
-  const hideNotification = () => {
-    setShowNotification(false);
-  };
+	const hideNotification = () => {
+		setShowNotification(false);
+	};
 
-  return (
-    <nav className="container">
-      <ul>
-        <li>
-          <Link to="/" className="brand">
-            <img src={logo} alt="logo Epik'Eco" />
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        {user ? (
-          <>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/profil">Profil</Link>
-            </li>
-            <li>
-              <a
-                href="/"
-                onClick={(e) => {
-                  e.preventDefault();
-                  tokenContext.onTokenChange(null);
-                  setShowNotification(true);
-                  setTimeout(hideNotification, 3000);
-                }}
-              >
-                Déconnexion
-              </a>
-            </li>
-          </>
-        ) : (
-          <li>
-            <Link to="/signin" role="button" className="outline">
-              Connexion
-            </Link>
-          </li>
-        )}
-      </ul>
-      {showNotification && (
-        <article className="info alert-popup">
-          A bientôt Epikopain(e)!
-        </article>
-      )}
-    </nav>
-  );
+	return (
+		<nav className="container">
+			<ul>
+				<li>
+					<Link to="/" className="brand">
+						<img src={logo} alt="logo Epik'Eco" />
+					</Link>
+				</li>
+			</ul>
+			<ul>
+				{user ? (
+					<>
+						<li>
+							<Link to="/dashboard">Challenges</Link>
+						</li>
+						<li>
+							<Link to="/profil">Compte</Link>
+						</li>
+						<li>
+							<a
+								href="/"
+								onClick={e => {
+									e.preventDefault();
+									tokenContext.onTokenChange(null);
+									setShowNotification(true);
+									setTimeout(hideNotification, 3000);
+								}}
+							>
+								Déconnexion
+							</a>
+						</li>
+					</>
+				) : (
+					<li>
+						<Link to="/signin" role="button" className="outline">
+							Connexion
+						</Link>
+					</li>
+				)}
+			</ul>
+			{showNotification && (
+				<article className="info alert-popup">A bientôt Epikopain(e)!</article>
+			)}
+		</nav>
+	);
 };
 
 export default Header;
--- src/pages/ChallengePage.tsx
+++ src/pages/ChallengePage.tsx
@@ -1,125 +1,116 @@
-import { useQuery } from "@apollo/client";
-import React, { useContext, useEffect, useState } from "react";
-import { IParticipantChallenge } from "../interfaces/IChallenge";
-import { readOneChallenge } from "../graphql/readOneChallenge";
-import { Navigate, useParams } from "react-router-dom";
-import { UserContext } from "../context/AuthProvider";
-import UpdateChallenge from "../components/UpdateChallenge";
-import ReadChallenge from "../components/ReadChallenge";
-import ActionsList from "../components/ActionsList";
-import UpdateActionsChallenge from "../components/UpdateActionsChallenge";
-import { Link } from "react-router-dom";
-import ChallengeLeaderboardPage from "./ChallengeLeaderboardPage";
+import { useQuery } from '@apollo/client';
+import React, { useContext, useEffect, useState } from 'react';
+import { IParticipantChallenge } from '../interfaces/IChallenge';
+import { readOneChallenge } from '../graphql/readOneChallenge';
+import { Navigate, useParams } from 'react-router-dom';
+import { UserContext } from '../context/AuthProvider';
+import UpdateChallenge from '../components/UpdateChallenge';
+import ReadChallenge from '../components/ReadChallenge';
+import ActionsList from '../components/ActionsList';
+import UpdateActionsChallenge from '../components/UpdateActionsChallenge';
+import { Link } from 'react-router-dom';
+import ChallengeLeaderboardPage from './ChallengeLeaderboardPage';
+import ChallengeNavigation from '../components/ChallengeNavigation';
 
 const ChallengePage = () => {
-  const user = useContext(UserContext);
-  const { challengeId, view } = useParams();
+	const user = useContext(UserContext);
+	const { challengeId, view } = useParams();
 
-  const [userStatus, setUserStatus] = useState<null | "participant" | "owner">(
-    null
-  );
-  const [userToChallengeId, setUserToChallengeId] = useState<string>("");
-  const [editableMode, setEditableMode] = useState<Boolean>(false);
-  const [editableActionsMode, setEditableActionsMode] = useState<Boolean>(
-    false
-  );
-  const [challenge, setChallenge] = useState<
-    null | IParticipantChallenge | undefined
-  >(undefined);
+	const [userStatus, setUserStatus] = useState<null | 'participant' | 'owner'>(
+		null,
+	);
+	const [userToChallengeId, setUserToChallengeId] = useState<string>('');
+	const [editableMode, setEditableMode] = useState<Boolean>(false);
+	const [editableActionsMode, setEditableActionsMode] = useState<Boolean>(
+		false,
+	);
+	const [challenge, setChallenge] = useState<
+		null | IParticipantChallenge | undefined
+	>(undefined);
 
-  const { data } = useQuery<{ readOneChallenge: IParticipantChallenge }>(
-    readOneChallenge,
-    {
-      variables: {
-        challengeId,
-      },
-    }
-  );
+	const { data } = useQuery<{ readOneChallenge: IParticipantChallenge }>(
+		readOneChallenge,
+		{
+			variables: {
+				challengeId,
+			},
+		},
+	);
 
-  useEffect(() => {
-    if (data && data.readOneChallenge) {
-      setChallenge(data.readOneChallenge);
-      for (const userToChallenge of data.readOneChallenge.userToChallenges) {
-        const userId = userToChallenge.user.id;
+	useEffect(() => {
+		if (data && data.readOneChallenge) {
+			setChallenge(data.readOneChallenge);
+			for (const userToChallenge of data.readOneChallenge.userToChallenges) {
+				const userId = userToChallenge.user.id;
 
-        if (user && user.id && userId === user.id) {
-          setUserStatus("participant");
-          setUserToChallengeId(userToChallenge.id);
+				if (user && user.id && userId === user.id) {
+					setUserStatus('participant');
+					setUserToChallengeId(userToChallenge.id);
 
-          if (data.readOneChallenge.createdBy.id === user.id) {
-            setUserStatus("owner");
-          }
-        }
-      }
-    }
-  }, [data, user, userStatus, userToChallengeId]);
+					if (data.readOneChallenge.createdBy.id === user.id) {
+						setUserStatus('owner');
+					}
+				}
+			}
+		}
+	}, [data, user, userStatus, userToChallengeId]);
 
-  if (challenge === null) {
-    return <Navigate to={"/dashboard"} replace={true} />;
-  } else if (challenge !== undefined && challenge !== null) {
-    return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="../infos" relative="path" role="button">
-                infos
-              </Link>
-            </li>
-            <li>
-              <Link to="../tasks" relative="path" role="button">
-                taches
-              </Link>
-            </li>
-            <li>
-              <Link to="../leaderboard" relative="path" role="button">
-                classement
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        {view === "infos" ? (
-          <>
-            {editableMode ? (
-              <UpdateChallenge
-                challenge={challenge}
-                toggleEditableMode={() => setEditableMode(false)}
-              />
-            ) : (
-              <ReadChallenge
-                challenge={challenge}
-                userToChallengeId={Number(userToChallengeId)}
-                toggleUserStatus={setUserStatus}
-                toggleEditableMode={setEditableMode}
-                userStatus={userStatus}
-              />
-            )}
-          </>
-        ) : view === "tasks" ? (
-          <>
-            {editableActionsMode === false ? (
-              <ActionsList
-                challenge={challenge}
-                userStatus={userStatus}
-                toggleEditableActionsMode={() => setEditableActionsMode(true)}
-              />
-            ) : (
-              <UpdateActionsChallenge
-                challenge={challenge}
-                toggleEditableActionsMode={() => setEditableActionsMode(false)}
-              />
-            )}
-          </>
-        ) : view === "leaderboard" ? (
-          <ChallengeLeaderboardPage />
-        ) : (
-          <Navigate to={"/dashboard"} replace={true} />
-        )}
-      </div>
-    );
-  } else {
-    return <div>Loading...</div>;
-  }
+	if (challenge === null) {
+		return <Navigate to={'/dashboard'} replace={true} />;
+	} else if (challenge !== undefined && challenge !== null) {
+		return (
+			<div>
+				{view === 'infos' ? (
+					<>
+						{editableMode ? (
+							<UpdateChallenge
+								challenge={challenge}
+								toggleEditableMode={() => setEditableMode(false)}
+							/>
+						) : (
+							<>
+								<ChallengeNavigation />
+								<ReadChallenge
+									challenge={challenge}
+									userToChallengeId={Number(userToChallengeId)}
+									toggleUserStatus={setUserStatus}
+									toggleEditableMode={setEditableMode}
+									userStatus={userStatus}
+								/>
+							</>
+						)}
+					</>
+				) : view === 'tasks' ? (
+					<>
+						{editableActionsMode === false ? (
+							<>
+								<ChallengeNavigation />
+								<ActionsList
+									challenge={challenge}
+									userStatus={userStatus}
+									toggleEditableActionsMode={() => setEditableActionsMode(true)}
+								/>
+							</>
+						) : (
+							<UpdateActionsChallenge
+								challenge={challenge}
+								toggleEditableActionsMode={() => setEditableActionsMode(false)}
+							/>
+						)}
+					</>
+				) : view === 'leaderboard' ? (
+					<>
+						<ChallengeNavigation />
+						<ChallengeLeaderboardPage />
+					</>
+				) : (
+					<Navigate to={'/dashboard'} replace={true} />
+				)}
+			</div>
+		);
+	} else {
+		return <div>Loading...</div>;
+	}
 };
 
 export default ChallengePage;
