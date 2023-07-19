30c712088a854a819022dede7343caba1a9e9215
g-emeline <emelineg@polydea.com>
2023-07-19 15:05:56 +0200

    changed tab style

--- src/components/ChallengeNavigation.tsx
+++ src/components/ChallengeNavigation.tsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
 import React from "react";
 
 const ChallengeNavigation = () => {
@@ -18,22 +17,22 @@ const ChallengeNavigation = () => {
       <Box sx={{ width: "100%", typography: "body1" }}>
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Link to="../infos" relative="path" role="button">
-                <Tab label="Item One" value="1" />
+            <TabList
+              onChange={handleChange}
+              aria-label="lab API tabs example"
+              className="tablist"
+            >
+              <Link to="../infos" relative="path">
+                <Tab label="Informations" value="1" />
               </Link>
-              <Link to="../tasks" relative="path" role="button">
-                <Tab label="Item Two" value="2" />
+              <Link to="../tasks" relative="path">
+                <Tab label="Eco-gestes" value="2" />
               </Link>
-              <Link to="../leaderboard" relative="path" role="button">
-                <Tab label="Item Three" value="3" />
+              <Link to="../leaderboard" relative="path">
+                <Tab label="Classement" value="3" />
               </Link>
             </TabList>
           </Box>
-
-          <TabPanel value="1">Item One</TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
         </TabContext>
       </Box>
     </nav>
