799f6f0c7cfac06e13a1b29a89d8a66709437af4
g-emeline <emelineg@polydea.com>
2023-07-19 10:32:55 +0200

    added tabs mui components to challenge navigation

--- src/components/ChallengeNavigation.tsx
+++ src/components/ChallengeNavigation.tsx
@@ -1,27 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import React from "react";
 
 const ChallengeNavigation = () => {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="../infos" relative="path" role="button">
-						Infos
-					</Link>
-				</li>
-				<li>
-					<Link to="../tasks" relative="path" role="button">
-						Taches
-					</Link>
-				</li>
-				<li>
-					<Link to="../leaderboard" relative="path" role="button">
-						Classement
-					</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <nav>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Link to="../infos" relative="path" role="button">
+                <Tab label="Item One" value="1" />
+              </Link>
+              <Link to="../tasks" relative="path" role="button">
+                <Tab label="Item Two" value="2" />
+              </Link>
+              <Link to="../leaderboard" relative="path" role="button">
+                <Tab label="Item Three" value="3" />
+              </Link>
+            </TabList>
+          </Box>
+
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
+    </nav>
+  );
 };
 
 export default ChallengeNavigation;
