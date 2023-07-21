4b2e8a20ea2ec506b164bccc60f08c83fa9b6458
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2023-07-21 11:59:00 +0200

    Add ui on profile component

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useEffect, useState } from 'react';
-import { useUser } from '../context/AuthProvider';
-import { useMutation } from '@apollo/client';
-import { editUser } from '../graphql/editUser';
-
+import { useEffect, useState } from "react";
+import { useUser } from "../context/AuthProvider";
+import { useMutation } from "@apollo/client";
+import { editUser } from "../graphql/editUser";
+import { FaPencilAlt } from "react-icons/fa";
+import { ArrowLeft, Save2 } from "react-bootstrap-icons";
+import "./Profil.scoped.css";
 
 const Profil = () => {
   const user = useUser();
@@ -25,14 +27,13 @@ const Profil = () => {
           },
         },
       });
-      console.log('Pseudo edited!');
+      console.log("Pseudo edited!");
       setIsEdited(false);
       setAlert(true);
     } catch (error) {
-      console.log('Error with pseudo edit:', error);
+      console.log("Error with pseudo edit:", error);
     }
   };
-
 
   useEffect(() => {
     if (alert === true) {
@@ -45,11 +46,11 @@ const Profil = () => {
   }, [alert]);
 
   return (
-    <div>
+    <article>
       <h1 className="title">Profil</h1>
       <p className="email">Email: {user.email}</p>
       <p>
-        Pseudo:{' '}
+        Pseudo:{" "}
         {isEdited ? (
           <div>
             <input
@@ -57,21 +58,36 @@ const Profil = () => {
               value={pseudo}
               onChange={(e) => setPseudo(e.target.value)}
             />
-            <button onClick={sendNewPseudo}>Valider</button>
           </div>
         ) : (
           user.name
         )}
       </p>
-      <button onClick={editPseudo}>
-        {isEdited ? 'Annuler' : 'Modifier pseudo'}
-      </button>
+      {isEdited ? (
+        <div className="button-container-multiple">
+          <button onClick={sendNewPseudo}>
+            <Save2 className="icone-btn" />
+            <span className="text-btn">Valider</span>
+          </button>
+          <button onClick={editPseudo} className="outline">
+            <ArrowLeft className="icone-btn" />
+            <span className="text-btn">Annuler</span>
+          </button>
+        </div>
+      ) : (
+        <div className="button-container">
+          <button onClick={editPseudo}>
+            <FaPencilAlt className="icone-btn" />
+            <span className="text-btn">Modifier pseudo</span>
+          </button>
+        </div>
+      )}
       {alert && (
         <article className="alert alert-popup">
           <p>Pseudo modifié avec succès.</p>
         </article>
       )}
-    </div>
+    </article>
   );
 };
 
